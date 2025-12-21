e4d47b44f57f98e74458ab297f7faa8a3837a8dc
Poojitha308-T <poojithathadiboyina@gmail.com>
2025-12-21 19:26:14 +0530

    React Application implemented using Context API

--- React Evaluation/Q1/src/App.jsx
+++ React Evaluation/Q1/src/App.jsx
@@ -1,8 +1,13 @@
 import PostsList from "./components/PostsList";
+import PostsContext from "./contexts/PostsContext";
 
 function App(){
     return(
+      <>
       <PostsList/>
+      <PostsContext/>
+      </>
+      
     )
 }
 export default App;
