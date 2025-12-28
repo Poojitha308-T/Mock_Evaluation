74026b96601684e93ceeb573c898f61e529d1ed8
Poojitha308-T <poojithathadiboyina@gmail.com>
2025-12-28 19:17:16 +0530

    Fleet_Management_App

---
+++ Mini_Fleet_Management_App/fleet-management-app/src/Admin.jsx
@@ -0,0 +1,7 @@
+function Admin(){
+    return(
+        <h2> Welcome to Admin Page</h2>
+    )
+
+}
+export default Admin;
---
+++ Mini_Fleet_Management_App/fleet-management-app/src/Login.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
+
+function Login(){
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [valid, setValid] = useState("false");
+    const navigate = useNavigate;
+    
+
+    useEffect=((email,password)=>{
+        const Email = "admin@gmail.com";
+        const Password = "admin1234";
+        if(email===Email && password=== Password){
+            setValid(true);
+        }
+        else{
+            setValid(false);
+        }
+    },[email,password])
+
+    const handlelogin=(e)=>{
+        if(valid){
+            alert("Login success")
+            navigate("/admin");
+        }
+        else{
+            alert("Wrong email or password");
+        }
+    }
+
+
+
+    return(
+        <div>
+            <input type="email" placeholder="Enter Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+            <br/>
+            <input type="password" placeholder="Enter Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            <br/>
+            <button onClick={handlelogin}>Login</button>
+        </div>
+    )
+}
+export default Login;
