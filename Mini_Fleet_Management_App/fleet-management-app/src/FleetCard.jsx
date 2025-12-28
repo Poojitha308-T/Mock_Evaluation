f2b68d0c9447a12b9aab9bae29affee21c4a1301
Poojitha308-T <poojithathadiboyina@gmail.com>
2025-12-28 20:34:55 +0530

    Fleet_Management_App

---
+++ Mini_Fleet_Management_App/fleet-management-app/src/FleetCard.jsx
@@ -0,0 +1,12 @@
+const FleetCard= React.memo(function FleetCard({
+    fleet,
+    updateDriver;
+    ToggleStatus;
+    onDelete;
+})
+return(
+    <div>
+    </div>
+)
+);
+export default FleetCard;
