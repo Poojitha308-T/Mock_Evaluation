c7cc95a63cb95f8e0a5838d09988a6ac2719e7c2
Poojitha308-T <poojithathadiboyina@gmail.com>
2025-12-28 20:09:12 +0530

    Fleet_Management_App

--- Mini_Fleet_Management_App/fleet-management-app/src/Admin.jsx
+++ Mini_Fleet_Management_App/fleet-management-app/src/Admin.jsx
@@ -8,13 +8,28 @@ function Admin(){
         status:""
     });
     const [fleets, setFleets] = useState([]);
+
+    const handlesubmit=(e)=>{
+        e.preventDefault();
+
+        setFleets([...fleets, form]);
+
+        setForm({
+            redNo:"",
+            category:"",
+            driver:"",
+            status:""
+        });
+    }
+
+    
     return(
         <div>
             <div>
                 <form>
                     <input type="text" placeholder="Vehicle Registration Number" value={form.redNo} onChange={(e)=>setForm(e.target.value)}/>
                     <br/><br/>
-                    <select name="category" value={form.category} onChange={}>
+                    <select name="category" value={form.category} onChange={(e)=>setForm(e.target.value)}>
                         <option value="All">Select category</option>
                         <option value="auto">Auto</option>
                         <option value="car">Car</option>
@@ -24,11 +39,11 @@ function Admin(){
 
                     <br/> <br/>
 
-                    <input type="text" placeholder="Driver name" value={form.driver} onChange={}/>
+                    <input type="text" placeholder="Driver name" value={form.driver} onChange={(e)=>setForm(e.target.value)}/>
 
                     <br/> <br/>
 
-                    <select name="status" value={form.status}>
+                    <select name="status" value={form.status} onChange={(e)=>setForm(e.target.value)}>
                         <option value="availability">Availability</option>
                         <option value="available">Available</option>
                         <option value="unavailable">Unavailable</option>
@@ -36,7 +51,7 @@ function Admin(){
 
                     <br/> <br/>
 
-                    <button>Add Fleet</button>
+                    <button onClick={handlesubmit}>Add Fleet</button>
 
                 </form>
             </div>
