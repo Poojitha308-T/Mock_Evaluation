d80d2494e76fac2924e99e54515cd95d390b4bde
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-18 19:39:49 +0530

    E-commerce Orders and Analytics API

--- E-Commerce App/Q1/index.js
+++ E-Commerce App/Q1/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const fs = require("fs");
+const productRoutes = require("./routes/products.routes")
+
 
 const app = express();
 app.use(express.json());
+
+app.use("/products", productRoutes);
+
 app.listen(3000, ()=>{
     console.log("Server running at http://localhost:3000");
 })
---
+++ E-Commerce App/Q1/routes/orders.routes.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+
+const readDB = () => JSON.parse(fs.readFileSync("db.json","utf-8"));
+
+const writeDB = (data) => fs.writeFileSync("db.json",JSON.stringify(data, null, 2));
+
+router.post("/",(req,res) => {
+    const { productId, quantity } = req.body;
+    const db = readDB();
+
+    const product = db.products.find(p => p.id === productId);
+    if(!product){
+        return res.status(404).json({msg: "Product not found"});
+    }
+
+    if(product.stock === 0 || quantity> product.stock){
+        return res.status(400).json({msg: "Insufficient stock"});
+    }
+
+    const totalAmount = product.price * quantity;
+
+    product.stock -= quantity;
+
+    const order = {
+        id: Date.now(),
+        productId,
+        quantity,
+        totalAmount,
+        status: "placed",
+        createdAt: new Date().toISOString().split("T")[0]
+    };
+
+    db.orders.push(order);
+    writeDB(db);
+
+    res.status(201).json(order);
+
+});
+
+router.get("/",(req,res) => {
+    const db = readDB();
+    res.json(db.orders);
+});
+
+router.delete("/:orderId", (req, res) => {
+    const db = readDB();
+    const order = db.orders.find( o =>o.id == req.params.orderId);
+
+    if(!order){
+        return res.status(404).json({msg: "Order Not Found"});
+    }
+
+    if(order.status === "cancelled"){
+        return res.status(400).json({msg: "Already cancelled"});
+    }
+
+    const today = new Date().toISOString().split("T")[0];
+    if(order.createdAt !== today){
+        return res.status(400).json("Cancellation not allowed")
+    }
+})
