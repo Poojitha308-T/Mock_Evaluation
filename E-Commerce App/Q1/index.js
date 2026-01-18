1a2780c1eb2720496b4798a19a8c2cb2236b6093
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-18 20:17:30 +0530

    E-commerce Orders and Analytics API

--- E-Commerce App/Q1/index.js
+++ E-Commerce App/Q1/index.js
@@ -2,14 +2,14 @@ import express, { json } from "express";
 import fs from "fs";
 import productRoutes from "./routes/products.routes.js";
 import orderRoutes from "./routes/orders.routes.js";
-
+import analyticsRoutes from "./routes/analytics.routes.js";
 
 const app = express();
 app.use(json());
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
-
+app.use("/analytics",analyticsRoutes);
 app.listen(3000, ()=>{
     console.log("Server running at http://localhost:3000");
 })
---
+++ E-Commerce App/Q1/routes/analytics.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import fs from "fs";
+const router = Router();
+
+const readDB = () =>JSON.parse(fs.readFileSync("db.json", "utf-8"));
+
+router.get("/allorders",(req,res) => {
+    const db = readDB();
+    res.json({
+        count: db.orders.length,
+        orders: db.orders
+    });
+});
+
+router.get("/cancelled-orders", (req,res) => {
+    const db = readDB();
+    const cancelled = db.orders.filter(o => o.status === "cancelled");
+});
+
+module.exports = router;
