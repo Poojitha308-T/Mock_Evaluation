cf1a9b1b4a45ae18e183b27f4baa9bd05fdff172
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-21 19:48:53 +0530

    E-commerce Orders & Analytics API

--- E-Commerce Orders/Q2/routes/orders.routes.js
+++ E-Commerce Orders/Q2/routes/orders.routes.js
@@ -30,6 +30,72 @@ router.post("/", (req,res) => {
 
     const totalAmount = product.price * quantity;
 
-    
+    const newOrder = {
+        id: Date.now(),
+        productId,
+        quantity,
+        totalAmount,
+        status: "placed",
+        createdAt: new Date(). toISOString().split("T")[0]
+    };
 
-})
+    product.stock -= quantity;
+    db.orders.push(newOrder);
+
+    writeDB(db);
+    res.status(201).json(newOrder);
+});
+
+router.get("/", (req,res) => {
+    const db = readDB();
+    const order = db.orders.find(o => o.id == req.params.orderId);
+
+    if(!order){
+        return res.status(404).json({message: "Order not found"});
+    }
+
+    if(order.status === "cancelled") {
+        return res.status(400).json({message: "Order already cancelled"});
+    }
+
+    const today = new Date().toISOString().split("T")[0];
+    if(order.createdAt !== today){
+        return res.status(400).json({ message: "Cancellation not allowed" });
+    }
+
+    const product = db.products.find(p =>p.id === order.productId);
+    product.stock += order.quantity;
+
+    order.status = "cancelled";
+    writeDB(db);
+
+    res.json({ message: "Order cancelled successfully" });
+});
+
+router.patch("/change-status/:orderId", (req,res) => {
+    const { status } = req.body;
+    const db = readDB();
+    const order = db.orders.find(o => o.id == req.params.orderId);
+
+    if(!order) {
+        return res.status(404).json({ message: "Order not found" });
+    }
+
+    if(["cancelled", "delivered"].includes(order.status)) {
+        return res.status(400).json({ message: "Status change not allowed" });
+    }
+
+    const flow = ["placed", "shipped", "delivered"];
+    const currentIndex = flow.indexOf(order.status);
+
+    if(flow[currentIndex + 1] !== status) {
+        return res.status(400).json({message: "Invalid status flow" });
+    }
+
+    order.status = status;
+    writeDB(db);
+
+    res.json(order);
+});
+
+export default router;
--- E-Commerce Orders/Q2/server.js
+++ E-Commerce Orders/Q2/server.js
@@ -1,11 +1,13 @@
 import express from "express";
-import productsRoutes from "./routes/products.routes";
-
+import productsRoutes from "./routes/products.routes.js";
+import orderRoutes from "./routes/orders.routes.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/products", productsRoutes);
+app.use("/orders", orderRoutes);
+
 
 app.listen(3000, ()=> {
     console.log("Server running on port 3000");
