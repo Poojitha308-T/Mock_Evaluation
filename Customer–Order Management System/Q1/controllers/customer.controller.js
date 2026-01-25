ad9553d3784cbb6f282e0bcc4a5922bd1b105b50
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-25 19:40:08 +0530

    Customer–Order Management System

---
+++ Customer–Order Management System/Q1/controllers/customer.controller.js
@@ -0,0 +1,19 @@
+import { supabase } from './../config/supabase.js';
+
+export const registerCustomer = async (req, res) => {
+    const { full_name, email, phone } = req.body;
+
+    if(!full_name || !email || !phone) {
+        return res.status(400).json({message: 'Missing required fields'});
+    }
+
+    const {data, error} = await supabase.from('customers').insert([{full_name, email, phone}]);
+
+    if(error){
+        if(error.code){
+            return res.status(409).json({message: 'Email already exists'})
+        }
+        return res.status(500).json({message: error.message})
+    }
+    res.status(201).jdon({message:'Customer registered',data})
+}
--- Customer–Order Management System/Q1/server.js
+++ Customer–Order Management System/Q1/server.js
@@ -1,2 +1,13 @@
 import express from 'express';
+import cors  from 'cors';
+import customerRoutes from './routes/customer.routes.js';
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/customers", customerRoutes);
+
+app.listen(3000, () => {
+    console.log("Server running");
+})
