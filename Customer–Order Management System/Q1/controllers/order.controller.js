59a18edca1a61c4fbea0e782cc3e3c2f95156b26
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-25 20:11:15 +0530

    Customer–Order Management System

--- Customer–Order Management System/Q1/controllers/order.controller.js
+++ Customer–Order Management System/Q1/controllers/order.controller.js
@@ -1,58 +1,93 @@
 import { supabase } from './../config/supabase';
 
-export const createOrder = async(req,res) => {
-    try{
+export const createOrder = async (req, res) => {
+    try {
         const { product_name, quantity, price, customerId } = req.body;
 
-        if(!product_name || !quantity || !price || !customerId) {
-            return res.status(400).json({msg:'Missing required fiels'})
+        if (!product_name || !quantity || !price || !customerId) {
+            return res.status(400).json({ msg: 'Missing required fiels' })
         }
 
         const { data, error } = await supabase
-        .from('orders')
-        .insert([{
-            product_name,
-            quantity,
-            price,
-            customer_id: customerId 
-        }])
+            .from('orders')
+            .insert([{
+                product_name,
+                quantity,
+                price,
+                customer_id: customerId
+            }])
 
-        if(error){
-            return res.status(400).json({msg: error.message })
+        if (error) {
+            return res.status(400).json({ msg: error.message })
         }
 
         return res.status(201).json({
-            message: 'Order created sucessfully'. data
+            message: 'Order created sucessfully'.data
         })
-    } catch(err){
-        return res.status(500).json({msg: err.messgae })
+    } catch (err) {
+        return res.status(500).json({ msg: err.messgae })
     }
 }
 
 //get customer orders
 
-export const getCustomerOrders = async(req,res) => {
-    try{
+export const getCustomerOrders = async (req, res) => {
+    try {
         const { customerId } = req.params
 
-        if(!customerId){
-            return res.status(400).json({msg: 'Customer id is required'})
+        if (!customerId) {
+            return res.status(400).json({ msg: 'Customer id is required' })
+        }
+
+        const { data, error } = await supabase
+            .from('orders')
+            .select('*')
+            .eq('customer_id', customerId)
+
+        if (error) {
+            return res.status(400).json({ msg: error.message })
+        }
+
+        return res.status(200).json(data)
+    } catch (err) {
+        return res.status(500).json({ msg: error.message })
+    }
+}
+
+
+// update order
+
+export const updateOrder = async (req, res) => {
+    try {
+        const { orderId } = req.params
+        const { quantity, price, order_status } = req.body
+
+        if(!orderId){
+            return res.status(400).json({msg: ' Order id is required'})
         }
 
         const { data, error } = await supabase
         .from('orders')
-        .select('*')
-        .eq('customer_id', customerId)
+        .update({ quantity, price, order_status })
+        .eq('id', orderId)
 
-        if(error){
-            return res.status(400).json({msg: error.message })
+        if(error) {
+            return res.status(400).json({msg:error.message})
         }
 
-        return res.status(200).json(data)
+        if(data.length === 0) {
+            return res.status(404).json({msg:'Order not found'})
+        }
+        return res.status(200).json({
+            msg: ' Order updated successfully ', data 
+        })
     } catch(err){
-        return res.status(500).json({msg: error.message})
+        return res.status(500).json({msg: err.message})
     }
 }
+
+// delete order
+
 
 
 
--- Customer–Order Management System/Q1/routes/order.routes.js
+++ Customer–Order Management System/Q1/routes/order.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createOrder, getCustomerOrders } from '../controllers/order.controller';
+import { createOrder, getCustomerOrders, updateOrder } from '../controllers/order.controller';
 
 const router = express.Router();
 
 router.post('/add-order', createOrder);
-router.get('/get-my-orders/:customerId', getCustomerOrders)
+router.get('/get-my-orders/:customerId', getCustomerOrders);
+router.put('/update-order/:orderId', updateOrder);
 
 export default router;
